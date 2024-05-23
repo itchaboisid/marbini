f27bf04647a2bd34f478c91a7dccf9c00c231163
Marvssssq <marvnmanzo@gmail.com>
2024-05-23 17:33:51 +0800

    updat

---
+++ server/index.php
@@ -0,0 +1,16 @@
+<?php
+require '../vendor/autoload.php';
+
+DB::$user = 'sql12708572';
+DB::$password = 'VJvLZLjbsE';
+DB::$dbName = 'sql12708572';
+DB::$host = 'sql12.freemysqlhosting.net';
+DB::$port = '3306';
+
+extract($_POST);
+
+DB::insert('tbl_registration', $_POST);
+
+echo json_encode(["response" => 1]);
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'sergeytsalkov/meekrodb' => array(
+            'pretty_version' => 'v2.5.1',
+            'version' => '2.5.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sergeytsalkov/meekrodb',
+            'aliases' => array(),
+            'reference' => '08b683a274365983aaf033cf9c589dcbe5190028',
+            'dev_requirement' => false,
+        ),
+    ),
+);
